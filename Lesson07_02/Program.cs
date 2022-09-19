2628ad808408996e0894e6a05539525a2919c65a
DaumaG <dgarnys18@gmail.com>
2022-09-19 18:56:46 +0300

    Add project files.

---
+++ Lesson07_02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lesson07_02
+{
+    internal class Program
+    {
+        public struct BibliotekosKnyga
+        {
+            public int Id;
+            public string Title;
+            public string ReaderName;
+            public DateTime TakenAt;
+
+            public BibliotekosKnyga(int id, string title, string readerName, DateTime takenAt)
+            {
+                Id = id;
+                Title = title;
+                ReaderName = readerName;
+                TakenAt = takenAt;
+            }
+
+            public int HowLongBookIsTaken()
+            {
+                DateTime currentDate = DateTime.Today;
+                int bookTakenInDays = (currentDate - TakenAt).Days;
+
+                return bookTakenInDays;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            BibliotekosKnyga bibliotekosKnyga = new BibliotekosKnyga(1, "Haris Poteris", "Daumantas", new DateTime(2022, 09, 09));
+
+            int bookTakenInDays = bibliotekosKnyga.HowLongBookIsTaken();
+
+            Console.WriteLine($"Knyga paimta {bookTakenInDays} dienu");
+        }
+    }
+}
